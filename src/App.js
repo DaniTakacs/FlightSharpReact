0905d0b57e7cd0215c1b86b87230b3caf51fbbfe
Dániel Takács <takacs.d24@gmail.com>
2020-07-30 10:35:57 +0200

    Connected the two functionalities

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Form from './components/Form';
-import FlightList from './components/FlightList';
+import Form from "./Components/Form";
+import FlightList from "./Components/FlightList";
 import axios from "axios";
 
 const App = (props) => {
   const [State, SetAppState] = useState({
     accessToken: "237f37871102101c4ec439ba6c98520e",
-    filteredFlights: {},  
+    filteredFlights: {},
   });
 
-  useEffect(()=>{getFlights("BUD", "BER", "HUF")},[])
+  useEffect(() => {}, []);
 
   let content;
 
   const getFlights = (from, to, currency) => {
     const axios = require("axios");
     axios({
-        "method":"GET",
-        "url":"https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap",
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-access-token":`${State.accessToken}`,
-        "x-rapidapi-host":"travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-        "x-rapidapi-key":"8cc5a982f3msha7010a92e8c909ap14602cjsneeae413a870c",
-        "useQueryString":true
-        },"params":{
-        "destination":`${to}`,
-        "origin":`${from}`,
-        "currency":`${currency}`,
-        "page":"None"
-        }
-        })
-        .then((response)=>{
-          const respFlights = response.data.data[`${to}`];
-          SetAppState({...State, filteredFlights:respFlights})
-        })
-        .catch((error)=>{
-          console.log(error)
-        })
+      method: "GET",
+      url:
+        "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-access-token": `${State.accessToken}`,
+        "x-rapidapi-host":
+          "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+        "x-rapidapi-key": "8cc5a982f3msha7010a92e8c909ap14602cjsneeae413a870c",
+        useQueryString: true,
+      },
+      params: {
+        destination: `${to}`,
+        origin: `${from}`,
+        currency: `${currency}`,
+        page: "None",
+      },
+    })
+      .then((response) => {
+        const respFlights = response.data.data[`${to}`];
+        SetAppState({ ...State, filteredFlights: respFlights });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  return <div className="App">
-    <div><Form></Form></div>
-     <FlightList></FlightList>
+  return (
+    <div className="App">
+      <div>
+        <Form
+          click={(from, to, currency) => getFlights(from, to, currency)}
+        ></Form>
+      </div>
+      <FlightList></FlightList>
       <div>{content}</div>
-  </div>;
+    </div>
+  );
 };
 
-export default App
+export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,59 +1,51 @@
-import React from 'react';
-import useInput from '../hooks/useInput';
-import './design.scss';
+import React from "react";
+import useInput from "../hooks/useInput";
+import "./design.scss";
 
+const Form = (props) => {
+  const [fromPlace, bindFromPlace, resetFromPlace] = useInput("");
+  const [toPlace, bindToPlace, resetToPlace] = useInput("");
 
-function Form() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const validFromLength = fromPlace.length === 3;
+    const validToLength = toPlace.length === 3;
+    const fromCharsOnly = !hasNumber(fromPlace);
+    const toCharsOnly = !hasNumber(toPlace);
 
-    const [fromPlace, bindFromPlace, resetFromPlace] = useInput('')
-    const [toPlace, bindToPlace, resetToPlace] = useInput('')
-    
-    const submitHandler = e => {
-        e.preventDefault()
-        const validFromLength = fromPlace.length === 3;
-        const validToLength = toPlace.length === 3;
-        const fromCharsOnly = !hasNumber(fromPlace);
-        const toCharsOnly = !hasNumber(toPlace);
-
-        if(validFromLength && validToLength && fromCharsOnly && toCharsOnly){
-            alert(`Travelling from ${fromPlace} to ${toPlace}.`)
-        }
-        else {
-            alert('Start end Destination must be 3 chars long and can not contain numbers')
-        }
-
-        resetFromPlace()
-        resetToPlace()
+    if (validFromLength && validToLength && fromCharsOnly && toCharsOnly) {
+      props.click(fromPlace, toPlace, "HUF");
+      console.log(props);
+    } else {
+      alert(
+        "Start end Destination must be 3 chars long and can not contain numbers"
+      );
     }
 
-    const hasNumber = (myString) => {
-        return /\d/.test(myString);
-    }
+    resetFromPlace();
+    resetToPlace();
+  };
 
-    return (
-        <div>
-            <form onSubmit={submitHandler}>
-                <div className="segment">
-                    <h1>FlightSharp</h1>
-                </div>
-                <label>
-                    <input
-                    placeholder="Going From"
-                    {...bindFromPlace}
-                    type="text"
-                    />
-                </label>
-                <label>
-                    <input
-                    placeholder="Destination"
-                    {...bindToPlace}
-                    type="text"
-                    />
-                </label>
-                <button>Search</button>
-            </form>
+  const hasNumber = (myString) => {
+    return /\d/.test(myString);
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="segment">
+          <h1>FlightSharp</h1>
         </div>
-    )
-}
+        <label>
+          <input placeholder="Going From" {...bindFromPlace} type="text" />
+        </label>
+        <label>
+          <input placeholder="Destination" {...bindToPlace} type="text" />
+        </label>
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
